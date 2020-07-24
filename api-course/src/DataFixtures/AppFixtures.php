b7a48f3e60ff3a9941316fdc605c1dc1c2183bb9
mhmdmrabet <58339336+mhmdmrabet@users.noreply.github.com>
2020-07-24 18:37:58 +0200

    Fixtures: fake users

--- api-course/src/DataFixtures/AppFixtures.php
+++ api-course/src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Customer;
 use App\Entity\Invoice;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Faker\Factory;
@@ -16,31 +17,45 @@ class AppFixtures extends Fixture
 
         $chrono = 1 ;
 
-        for ($c = 0 ; $c < 30 ; $c++ ){
-            $customer = new Customer();
-            $customer->setFirstName($faker->firstName)
-                     ->setLastName($faker->lastName)
-                     ->setCompany($faker->company)
+        for ($u=0;$u<10;$u++){
+
+            $user = new User();
+            $user->setFirstName($faker->firstName)
+                 ->setLastName($faker->lastName)
+                ->setEmail($faker->email)
+                ->setPassword("password");
+
+            $manager->persist($user);
+
+            for ($c = 0 ; $c < mt_rand(5,20) ; $c++ ){
+
+                $customer = new Customer();
+                $customer->setFirstName($faker->firstName)
+                    ->setLastName($faker->lastName)
+                    ->setCompany($faker->company)
                     ->setEmail($faker->email);
 
-            $manager->persist($customer);
+                $manager->persist($customer);
 
-            for ($i = 0 ; $i < mt_rand(3,10) ; $i++){
-                $invoice = new Invoice();
-                $invoice->setAmount($faker->randomFloat(2, 250 , 5000))
+                for ($i = 0 ; $i < mt_rand(3,10) ; $i++){
+                    $invoice = new Invoice();
+                    $invoice->setAmount($faker->randomFloat(2, 250 , 5000))
                         ->setSentAt($faker->dateTimeBetween('-6 months'))
                         ->setStatus($faker->randomElement(['SENT', 'PAID' , 'CANCELLED']))
                         ->setCustomer($customer)
                         ->setChrono($chrono);
 
-                $chrono++ ;
+                    $chrono++ ;
 
-                $manager->persist($invoice);
+                    $manager->persist($invoice);
+                }
+
+                $manager->flush();
+
             }
-
-            $manager->flush();
-
         }
+
+
 
         $manager->flush();
     }
