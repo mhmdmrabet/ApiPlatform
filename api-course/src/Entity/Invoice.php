46b4bd9c6a63e7092373e6e0d413c6a455965459
mhmdmrabet <58339336+mhmdmrabet@users.noreply.github.com>
2020-07-29 18:08:40 +0200

    Sérialisation des ressources liées + Gestion du problème de la boucle infini

--- api-course/src/Entity/Invoice.php
+++ api-course/src/Entity/Invoice.php
@@ -52,6 +52,7 @@ class Invoice
     /**
      * @ORM\ManyToOne(targetEntity=Customer::class, inversedBy="invoices")
      * @ORM\JoinColumn(nullable=false)
+     * @Groups("invoices_read")
      */
     private $customer;
 
