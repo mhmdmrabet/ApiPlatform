60ff5d65cc0688027aa09c01cd661bdd819a37d5
mhmdmrabet <58339336+mhmdmrabet@users.noreply.github.com>
2020-08-08 17:30:52 +0200

    Création de la page de liste des Customers

--- api-course/assets/js/pages/CustomersPage.jsx
+++ api-course/assets/js/pages/CustomersPage.jsx
@@ -3,7 +3,43 @@ import React from 'react';
 const CustomersPage = (props) => {
     return (
         <div>
-            <h1>Mes clients</h1>
+
+            <h1 className="text-center">Liste des clients</h1>
+
+            <table className="table table-hover">
+
+                <thead>
+                    <tr>
+                        <th>ID.</th>
+                        <th>Client</th>
+                        <th>Email</th>
+                        <th>Entreprise</th>
+                        <th className="text-center">Factures</th>
+                        <th className="text-center">Montant total</th>
+                        <th></th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>19</td>
+                        <td>
+                            <a href="#">Mohamed M'rabet</a>
+                        </td>
+                        <td>mohamed@sym.fr</td>
+                        <td>Mohamed Inc</td>
+                        <td className="text-center">4</td>
+                        <td className="text-center">2 487,00 €</td>
+                        <td>
+                            <button className="btn btn-sm btn-danger">
+                                Supprimer
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+
+            </table>
+
         </div>
     );
 };
