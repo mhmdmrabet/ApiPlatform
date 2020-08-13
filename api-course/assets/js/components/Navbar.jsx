af49ee11b765e52628006ffcfcafc1c286f9ed5b
mhmdmrabet <58339336+mhmdmrabet@users.noreply.github.com>
2020-08-13 21:38:31 +0200

    Mise en place de lien de navigation dans la NavBar

--- api-course/assets/js/components/Navbar.jsx
+++ api-course/assets/js/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import authAPI from '../services/authAPI';
+import { NavLink } from "react-router-dom" ;
 
 const Navbar = (props) => {
 
@@ -10,7 +11,9 @@ const Navbar = (props) => {
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-                <a className="navbar-brand" href="#">SymReact</a>
+                <NavLink className="navbar-brand" to="/">
+                    SymReact
+                </NavLink>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01"
                         aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -19,22 +22,26 @@ const Navbar = (props) => {
                 <div className="collapse navbar-collapse" id="navbarColor01">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Clients</a>
+                            <NavLink className="nav-link" to="/customers">
+                                Clients
+                            </NavLink>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Factures</a>
+                            <NavLink className="nav-link" to="/invoices">
+                                Factures
+                            </NavLink>
                         </li>
                     </ul>
                     <ul className="navbar-nav ml-auto">
                         <li className="nav-item">
-                            <a href="" className="nav-link">
+                            <NavLink to="/register" className="nav-link">
                                 Inscription
-                            </a>
+                            </NavLink>
                         </li>
                         <li className="nav-item">
-                            <a href="" className="btn btn-secondary mr-2">
+                            <NavLink to="/login" className="btn btn-secondary mr-2">
                                 Connexion
-                            </a>
+                            </NavLink>
                         </li>
                         <li className="nav-item">
                             <button href="" className="btn btn-danger" onClick={handleLogout}>
